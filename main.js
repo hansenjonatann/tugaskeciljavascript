479a29e7d022c86a914bc04127821faba97d7890
hansenjonatann <hansenjonatann@gmail.com>
2023-02-10 21:36:55 +0700

    memperbaiki kesalahan pada sistem

--- main.js
+++ main.js
@@ -8,25 +8,25 @@ const hari = new Date().getDay()
 
 switch(hari) {
     case 1:
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Senin`)
     break
     case 2:
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Selasa`)
     break
     case 3:
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Rabu`)
     break
     case 4:
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Kamis`)
     break
     case 5: 
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Jumat`)
     break
     case 6:
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Sabtu`)
     break
     case 7:
-    alert(`Hari ini adalah hari ${hari}`)
+    alert(`Hari ini adalah hari Minggu`)
     break
     default:
     alert('Hari')
